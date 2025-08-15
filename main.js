f56c28c18dc2d3c6c450cb7352bf495dda5acecd
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-08-15 11:27:52 +0200

    Er i gang med fetch delen.

---
+++ main.js
@@ -0,0 +1,12 @@
+const pageWrapper = document.querySelector(".page-wrapper")
+const intro = pageWrapper.querySelector(".intro")
+const main = pageWrapper.querySelector(".main")
+const sidebar = pageWrapper.querySelector(".sidebar")
+const data = ""
+console.log(
+    fetch(data)
+
+);
+
+
+
