2d41ae1071fdd7dbca6e9056415c84e57f3d63fc
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-08-15 13:44:05 +0200

    Er færdig med fetch delen.

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ const pageWrapper = document.querySelector(".page-wrapper")
 const intro = pageWrapper.querySelector(".intro")
 const main = pageWrapper.querySelector(".main")
 const sidebar = pageWrapper.querySelector(".sidebar")
-const data = ""
-console.log(
-    fetch(data)
+const url = "/data/data.json"
 
-);
+fetch(url)
+    .then(respons => {
+        return respons.json()
+    })
+    .then(data => {
+        console.log(data);
 
+    })
 
 
